0a5dc939869aa267ed88b1da49d24c2fd1a0bbf6
Wyatt Bentley <wbent280@evit.com>
2021-09-20 13:05:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-import
+# Wyatt Bentley
+# 9-20-21
+# Web Scraping EVIT
+import requests
+import bs4
+request = requests.get("https://evit.com/high_school_career_training/hs")
+soup = bs4.BeautifulSoup(request.text, 'html.parser')
+prognames = soup.select('#ctl00_ContentPlaceHolder1_ctl11_divContent > ul')
+for elem in prognames:
+    print(elem.getText())
